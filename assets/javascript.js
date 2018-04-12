9d0ee575606319fd7be1c9062b0f44c916bd76ef
Jessica La Torre <jess.latorre@gmail.com>
2018-04-12 16:04:23 -0500

    created variables for childSnapshots. Started created variables for calculating time difference, remaining, etc. Need to console log to test, so results can be appended to table in DOM

--- assets/javascript.js
+++ assets/javascript.js
@@ -8,46 +8,93 @@ $(document).ready(function () {
         projectId: "train-scheduler-f1aee",
         storageBucket: "train-scheduler-f1aee.appspot.com",
         messagingSenderId: "664303252169"
-      };
-      firebase.initializeApp(config);
+    };
+    firebase.initializeApp(config);
 
-// created variable to represent the database
-var database = firebase.database ();
+    // created variable to represent the database
+    var database = firebase.database();
 
-//create an event listener to capture new train after user clicks button to submit
-$('#submitNewTrain').on('click', function(event) {
-// event.preventDefault();
-console.log("button clicked");
+    //create an event listener to capture new train after user clicks button to submit
+    $('#submitNewTrain').on('click', function (event) {
+        // event.preventDefault(); //not needed sine button type is "button" not "submit"
+        // console.log("button clicked");
 
-//store user input in variables; trim any extra spaces
-var trainName = $('#trainName').val().trim();
-var destination = $('#destination').val().trim();
+        //store user input in variables; trim any extra spaces
+        var trainName = $('#trainName').val().trim();
+        var frequency = $('#frequency').val().trim();
+        var destination = $('#destination').val().trim();
+        //goal of moment is to calc time using the year method for the correct timezone that user needs
+       
+        var firstTrainTime = $('#firstTrainTime').val().trim();
+        // console.log(moment().format());
+        var convertedTime = moment(firstTrainTime, "hh:mm").subtract(1, "years").format("X");
+        console.log.apply(convertedTime);
 
-//goal of moment is to calc time using the year method for the correct timezone that user needs
-var firstTrainTime = $('#firstTrainTime').val().trim();
-console.log (moment().format()); 
-// var convertedTime = moment(firstTrainTime, "hh:mm").subtract(1, "years").format("X");
-// console.log.apply(convertedTime);
+        // create local temporary object for holding new train data
+        var newTrain = {
+            Name: trainName,
+            Destination: destination,
+            Time: firstTrainTime,
+            frequency: frequency,
+        };
 
-var frequency = $('#frequency').val().trim();
+        //push adds user data to firebase db
+        database.ref().push(newTrain);
+        // console.log(trainName);
+        // console.log(destination);
+        // console.log(firstTrainTime);
+        // console.log(frequency);
 
-// create local temporary object for holding new train data
-var newTrain = {
-    Name: trainName,
-    Destination: destination,
-    Time: firstTrainTime,
-    frequency: frequency,
-};
+        // alert('train added!');
 
-database.ref().push(newTrain);
-console.log(trainName);
-console.log(destination);
-console.log(firstTrainTime);
-console.log(frequency);
+        //clear out user input boxes
+        $('#trainName').val('');
+        $('#destination').val('');
+        $('#firstTrainTime').val('');
+        $('#frequency').val('');
+    }) //end of onclick event
 
-// alert('train added!');
-})
-})
+    //Firebase event with child added retrieves data on each new post added to your app.
+    //The child_added event is typically used when retrieving a list of items from the database. Unlike value which returns the entire contents of the location, child_added is triggered once for each existing child and then again every time a new child is added to the specified path. The event callback is passed a snapshot containing the new child's data. For ordering purposes, it is also passed a second argument containing the key of the previous child.
+    database.ref().on('child_added', function(childSnapshot, prevChildKey) {
+        console.log(childSnapshot.val());
+
+    //store childSnapshots in variables
+    var trainName = childSnapshot.val().trainName;
+    var destination = childSnapshot.val().destination;
+    var firstTrainTime = childSnapshot.val().firstTrainTime;
+    var frequency = childSnapshot.val().frequency;
+
+        // console.log(trainName);
+        // console.log(destination);
+        // console.log(firstTrainTime);
+        // console.log(frequency);
+    })
+
+    //store user input in hh:mm format into new trainTime variable
+    var trainTime = moment.unix(firstTrainTime).format('hh:mm');
+    console.log("this is the train time: " + trainTime);
+
+    // crete new variable storing the difference between the times?
+    var timeDifference = moment().diff(moment(trainTime),'minutes');
+
+    // store only the remainder of timeDifference divided by frequency in variable
+    var remainingTime = timeDifference % frequency;
+
+    //store minutes until next arrival in new variable
+    var minutesUntilArrival = frequency - remainingTime;
+
+    //add minutes Until arrival to current time to get New arrival time in hh:mm; store in new var
+    var nextArrival = moment().add(minutesUntilArrival, "minutes").format('hh:mm');
+    console.log("this is the arrival", nextArrival);
+
+    //Dynamically append user input to table in DOM
+    $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td>" + minutesUntilArrival + "</td></tr>");
+
+
+}) //end of document.ready function
+
+
 
 //time now
 // console.log("CURRENT TIME: " +  moment(currentTime).format("hh:mm"));
